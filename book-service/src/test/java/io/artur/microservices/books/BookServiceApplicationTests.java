2e3af9b866672d7794c5dd6eb53d6db8585318b3
Artur Przychodzen <artur2p@gmail.com>
2022-11-07 20:27:30 +0100

    init

---
+++ book-service/src/test/java/io/artur/microservices/books/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artur.microservices.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
